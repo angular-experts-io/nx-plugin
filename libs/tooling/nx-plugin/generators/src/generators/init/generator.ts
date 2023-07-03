e070aadc8401dd37f6409f31bc4e32134c76ff14
kreuzerk <kevin.kreuzer90@icloud.com>
2023-07-03 14:08:53 +0200

    refactor: 💡 rename init generator function

--- libs/tooling/nx-plugin/generators/src/generators/init/generator.ts
+++ libs/tooling/nx-plugin/generators/src/generators/init/generator.ts
@@ -5,7 +5,7 @@ import {createAndGetConfigFileIfNonExisting} from "@ax/tooling/nx-plugin/config"
 
 import {InitSchema} from './schema';
 
-export default async function move(tree: Tree, schema: InitSchema) {
+export default async function init(tree: Tree, schema: InitSchema) {
   const {scopes, prefix, appSuffix} = schema;
   await createAndGetConfigFileIfNonExisting(tree, {
     scopes,
